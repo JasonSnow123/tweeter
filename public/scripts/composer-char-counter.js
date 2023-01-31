7df512ce2b17dd087316f9edbffc3607d4b19210
JasonSnow123 <m.jason.2018@gmail.com>
2023-01-31 16:02:25 -0500

    inserts character counter for tweets and changes font color when over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+$(document).ready(function() {
+  $("#tweet-text").on('keyup', function() {
+    let characterCount = this.value.length;
+    const counterDiv = $(this).parents().children(".tweet-options")[0];
+    let counterValue = counterDiv.lastElementChild;
+    counterValue.innerText = 140 - characterCount;
+    if (counterValue.innerText < 0) {
+      counterValue.classList.add('overlimit');
+    }
+    if (counterValue.innerText > 0) {
+      counterValue.classList.remove('overlimit');
+    }
+  });
+});
+
