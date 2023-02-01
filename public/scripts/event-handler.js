5a1e701b44b9015f660ef6d16f726ff5cbd6a433
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-01 17:26:25 -0500

    creates alert errors based on tweet text if empty or overlimit
    clears text field when submitting tweet

--- public/scripts/event-handler.js
+++ public/scripts/event-handler.js
@@ -2,6 +2,14 @@ $(() => {
   $("#create-tweet").on('submit', function(event) {
     event.preventDefault();
     const tweetData = $(this).serialize();
+    const tweetValue = tweetData.split("=")[1];
+    if (!tweetValue) {
+      return alert("Empty Tweet");
+    }
+    if (tweetValue.length > 140) {
+      return alert("Tweet content too long");
+    }
+    $("#tweet-text").val("");
     $.post("/tweets", tweetData);
   });
 });
