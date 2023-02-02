1d1091cde0a0edf6136774f532327abce4746dac
JasonSnow123 <m.jason.2018@gmail.com>
2023-02-01 19:18:46 -0500

    shows an error message instead of alert upon tweet creation error

--- public/scripts/event-handler.js
+++ public/scripts/event-handler.js
@@ -1,18 +1,26 @@
 $(() => {
   $("#create-tweet").on('submit', function(event) {
     event.preventDefault();
+    // formats the tweet description to become query string
     const tweetData = $(this).serialize();
     const tweetValue = tweetData.split("=")[1];
     if (!tweetValue) {
-      return alert("Empty Tweet");
+      return $('#error-tweet').slideDown(1000);
     }
     if (tweetValue.length > 140) {
-      return alert("Tweet content too long");
+      return $('#error-tweet').slideDown(1000);
     }
+    $('#error-tweet').slideUp(0);
+    // gets rid of the error message if the error checks pass
     $("#tweet-text").val("");
+    /* posts to the tweets url without refreshing the page
+     * uses a callback once the post requests is successful to get the last tweet
+     * posts the tweet as the first element in the container
+     */
     $.post("/tweets", tweetData, () => {
       $.get("/tweets", (data) => {
-        $("#tweet-container").prepend(createTweetElement(data[data.length - 1]));
+        const newestTweet = createTweetElement(data[data.length - 1]);
+        $("#tweet-container").prepend(newestTweet);
       });
     });
   });
